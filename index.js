38c9a4906be4fafe12384bf05f29753518cf7462
Ruben Reina <ruben.reina@perfectchannel.com>
2019-06-12 12:36:06 +0200

    Comment a change to check if the flickering issue on iPad is fixed

--- index.js
+++ index.js
@@ -305,7 +305,7 @@ export default class Drawer extends Component {
     let inMask = this.testPanResponderMask(e, gestureState)
     if (!inMask) return false
     // skip gesture process if we have mostly vertical swipe
-    if (!this._open && Math.abs(gestureState.dy) >= Math.abs(gestureState.dx)) return false
+    // if (!this._open && Math.abs(gestureState.dy) >= Math.abs(gestureState.dx)) return false
     this._panStartTime = Date.now()
     if (inMask && this.shouldCaptureGestures()) return true
     if (this.props.negotiatePan) return false
